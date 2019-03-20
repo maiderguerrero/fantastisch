9a67608ca20e8b1d6829502e29713d2550ceb42e
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2019-03-20 11:15:15 +0100

    Update style.js

--- style.js
+++ style.js
@@ -31,6 +31,9 @@
 				{ name: 'Lista desordenada 3', element: 'ul', attributes: { 'class': 'bck-ul bck-ul3' } },
 				{ name: 'Lista desordenada 4', element: 'ul', attributes: { 'class': 'bck-ul bck-ul4' } },
 
+				{ name: 'Celda 1', element: 'td', attributes: { 'class': 'bck-td-1' } },
+				{ name: 'Celda 2', element: 'td', attributes: { 'class': 'bck-td-2' } },
+
 				{ name: 'Caja 1', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box1' } },
 				{ name: 'Caja 2', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box2' } },
 				{ name: 'Caja 3', type: 'widget', widget: 'blink_box', attributes: { 'class': 'bck-box bck-box3' } },
